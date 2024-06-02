e7a94eb047c0fa94951182e71e6edf2b35516fdd
LeeYoungKyung <128415146+LeeYoungKyung@users.noreply.github.com>
2024-06-03 01:31:04 +0900

    [design]writePage

--- app/write/page.js
+++ app/write/page.js
@@ -1,12 +1,18 @@
 export default function Write() {
   return (
     <div className='write-css'>
-      <h4>write</h4>
-      <form action='/api/post/new' method='POST'>
-        <input name='title' placeholder='title'></input>
-        <input name='content' placeholder='content'></input>
+      <h4>daily</h4>
+      <form action='/api/post/new' method='POST' className='w-[80%]'>
+        <input name='title' placeholder='title' className='titleInput'></input>
+        <input
+          name='content'
+          placeholder='content'
+          className='contentInput'
+        ></input>
         {/* post랑 get만 사용가능  */}
-        <button type='submit'>button</button>
+        <button type='submit' className=' hover:bg-slate-300'>
+          작성
+        </button>
       </form>
     </div>
   );
