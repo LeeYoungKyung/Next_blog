baef73a8f9ee30fe8bc36c044c1e38eb046a3f3a
LeeYoungKyung <128415146+LeeYoungKyung@users.noreply.github.com>
2024-06-05 18:23:59 +0900

    [add]session

--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -1,6 +1,15 @@
 import { connectDB } from '@/util/database';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
 
 export default async function handler(요청, 응답) {
+  let session = await getServerSession(요청, 응답, authOptions);
+
+  if (session) {
+    요청.body.author = session.user.email;
+  }
+  console.log(요청.body);
+  //지금 로그인한 유저를 알 수 있음
   if (요청.method == 'POST') {
     if (요청.body.title == '') {
       return 응답.status(500).json('값을 입력하세요');
